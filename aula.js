cea97a9cfc7e513456a62ceb36e1cf95390bd33d
linconulian <lincon.lut@gmail.com>
2022-11-01 21:25:23 -0400

    Blew up the repository

---
+++ aula.js
@@ -0,0 +1,17 @@
+//Declaração de Variáveis
+
+let variavel = 10;
+variavel = 20; //let me permite alterar valores
+const pi = 3.14; //const não consigo altera valor (constante)
+console.log(variavel);
+//----------------------------------------------------------
+
+// Declaração de variáveis com operadores matemáticos:
+variavel = 10*10;
+console.log(variavel);
+variavel = 10/10;
+console.log(variavel);
+variavel = 10-10;
+console.log(variavel);
+//---------------------------------------------------------
+//10:25 min
