23cb28b2950ed0ab1438dab677499a5d56945a8c
linconulian <lincon.lut@gmail.com>
2022-12-12 09:01:37 -0400

    Challenge finished

--- Operadores/desafio.js
+++ Operadores/desafio.js
@@ -7,15 +7,23 @@
 
 //  Imprima no console o valor que será gasto para realizar a viagem.
 
+const precoCombEtanol = 5.1;
+const precoCombGasolina = 5.4;
+const kmPorL = 14.5;
+const viagemKm = 600;
+const tipoComb = "Gasolina";
 
-const precoComb = 5.79;
-const kmPorL = 10;
-const viagemKm = 100;
-console.log(`O preço do combustível será considerado a ${precoComb}, nosso carro fará ${kmPorL}km por litro e nossa viagem terá ${viagemKm} kilometros. `);
-
+if(tipoComb==="Etanol"){
 var litros = viagemKm / kmPorL;
-var gasto = litros*precoComb;
+var gasto = litros * precoCombEtanol;
 
-console.log(`Neste caso, serão ${litros.toFixed(2)} litros consumidos e nosso gasto em reais será R$ ${gasto.toFixed(2)}`);
+console.log(
+  `Neste caso, serão ${litros.toFixed(2)} litros consumidos de ${tipoComb} e nosso gasto em reais será R$ ${gasto.toFixed(2)}`
+);
+  } else if (tipoComb==="Gasolina") {
+    var litros = viagemKm / kmPorL;
+    var gasto = litros * precoCombGasolina;
+    console.log(`Neste caso, serão ${litros.toFixed(2)} litros consumidos de Gasolina e nosso gasto em reais será R$ ${gasto.toFixed(2)}`)
 
+  }
 //Fim do desafio
